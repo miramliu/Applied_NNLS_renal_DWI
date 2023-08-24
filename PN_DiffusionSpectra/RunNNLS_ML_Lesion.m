363f0abffbebad78e1d6a7fc76748ca5120dae55
Mira Liu <30845365+miramliu@users.noreply.github.com>
2023-08-24 11:11:02 -0400

    Update RunNNLS_ML_Lesion.m

--- PN_DiffusionSpectra/RunNNLS_ML_Lesion.m
+++ PN_DiffusionSpectra/RunNNLS_ML_Lesion.m
@@ -61,26 +61,7 @@ function [OutputDiffusionSpectrum, Chi, Resid, y_recon, resultsPeaks] = RunNNLS_
     %so gonna say there are < 3 peaks, to then split them up as fast if > 10, med if 10 > x > 2, and slow if < 2? 
     
     if nnz(~resultsPeaks) > 0 
-        dummyresultspeaks = resultsPeaks;
-        idx = find(resultsPeaks);
-        correctthese = idx(idx>3); %get the diffusion coefficients that are not zero
-        for k =1:numel(correctthese) %for the diffusion coefficients that need to be shuffed appropriately
-            j = correctthese(k);
-            n = DetermineComponent(resultsPeaks(j)); %see the speed of this peak
-            if n ~= j-3 %if it needs to be moved to a different speed fraction
-                %move to the correct fraction
-                dummyresultspeaks(n) = resultsPeaks(j-3);
-                dummyresultspeaks(n+3) = resultsPeaks(j);
-                %make the one it was moved from zero now
-                if dummyresultspeaks(j-3) == resultsPeaks(j-3) %if it has NOT been replaced by a prevoius move 
-                    dummyresultspeaks(j-3) = 0; %set to zero
-                    dummyresultspeaks(j) = 0;
-                end
-                
-            end
-            %pause()
-        end
-        resultsPeaks = dummyresultspeaks;
+        resultsPeaks = RecompartmentalizeMultiexponential(resultsPeaks);
     end
     
 
