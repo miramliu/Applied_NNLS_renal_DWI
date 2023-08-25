f71120e9435e47405c9ab0da933d64772e62f767
Mira Liu <30845365+miramliu@users.noreply.github.com>
2023-08-25 18:51:37 -0400

    added 3mo diffusion spectra

--- PN_DiffusionSpectra/DiffusionSpec_Anatomic.m
+++ PN_DiffusionSpectra/DiffusionSpec_Anatomic.m
@@ -6,6 +6,9 @@
 % in excel, can make new correctly labeled ROIs with new column from names see example below
 % sort alphabetically (data, sort, column a, alphabetically)
 % %= LEFT(cell,5) & "RK_" & RIGHT(left,LEN(cell)-5) #this would be to add RK to each of the ROIs
+
+%% also note: change read in runNNLS_ML to 3mo to have it read and save in correct folder. 
+
 function DiffusionSpec_Anatomic(PatientNum)
 
 %if both left and right
@@ -13,16 +16,25 @@ RoiTypes = {'LK_LP_C','LK_LP_M','LK_MP_C','LK_MP_M','LK_UP_C','LK_UP_M','RK_LP_C
 %if only right or left
 %RoiTypes = {'LP_C','LP_M','MP_C','MP_M','UP_C','UP_M'};
 
+%for 3mo, when there's only left or right: 
+% If only left
+%RoiTypes = {'LK_LP_C','LK_LP_M','LK_MP_C','LK_MP_M','LK_UP_C','LK_UP_M'};
+% If only right
+%RoiTypes = {'RK_LP_C','RK_LP_M','RK_MP_C','RK_MP_M','RK_UP_C','RK_UP_M'};
+
+
 for i = 1:length(RoiTypes)
     ROItype = RoiTypes{i}
 
+    %% change line 27 in runnnls_ml to ReadpatientDWIData_3mo
     [~, rsq, ~, ~, resultsPeaks] = RunNNLS_ML(PatientNum,ROItype);
 
     %plot(OutputDiffusionSpectrum);
     %pause(1)
 
-    pathtodata = '/Users/miraliu/Desktop/ML_PartialNephrectomy_Export';
-    ExcelFileName=[pathtodata, '/','PN_IVIM_DiffusionSpectra.xlsx']; % All results will save in excel file
+    % for 3mo
+    pathtodata = '/Users/miraliu/Desktop/ML_PartialNephrectomy_Export_3mo';
+    ExcelFileName=[pathtodata, '/','PN_IVIM_DiffusionSpectra_3mo.xlsx']; % All results will save in excel file
 
     Identifying_Info = {['PN_' PatientNum], ROItype};
     Existing_Data = readcell(ExcelFileName,'Range','A:B'); %read only identifying info that already exists
--- PN_DiffusionSpectra/RunNNLS_ML.m
+++ PN_DiffusionSpectra/RunNNLS_ML.m
@@ -24,7 +24,8 @@ function [OutputDiffusionSpectrum, rsq, Resid, y_recon, resultsPeaks] = RunNNLS_
     resultsPeaks = zeros(6,1); %6 was 9 before? unsure why
 
     ROItype = [PatientNum '_' ROItype];
-    SignalInput = ReadPatientDWIData(PatientNum, ROItype);
+    %% CHANGE HERE FOR BASELINE, 3M0 OR 12MO
+    SignalInput = ReadPatientDWIData_3mo(PatientNum, ROItype);
 
     %to match bi-exp, normalizing to b0
     SignalInput = SignalInput(:)/SignalInput(1);
@@ -90,3 +91,29 @@ function SignalInput = ReadPatientDWIData(PatientNum, ROItype)
 
 end
 
+% to be able to get the data for the DWI analysis... hopefully.
+function SignalInput = ReadPatientDWIData_3mo(PatientNum, ROItype)
+
+    pathtodata = '/Users/miraliu/Desktop/ML_PartialNephrectomy_Export_3mo';
+    pathtoCSV = [pathtodata '/' PatientNum '/' PatientNum '_Scan2.csv'];
+    
+    %read data
+    DataFrame = readtable(pathtoCSV,'PreserveVariableNames', true, 'Range','A:E','Delimiter', ',');    
+    ROITypeTable = DataFrame(startsWith(DataFrame.RoiName, ROItype),:);
+    SignalInput = zeros(9,1);
+    %average all four ROIs for analysis (CHECK IF I SHOULD DO THIS)
+    for k = 1:4 %for each of the 4 ROIs of every type (%%CHECK!!!!!!)
+        ROITypeTablesub = ROITypeTable(strcmp(ROITypeTable.RoiName, ROItype + string(k)),:); %so for example you want LK_LP_C, will check LK_LP_C1, LK_LP_C2 etc.
+        
+        % also make sure b-values are in order
+        if ROITypeTablesub.Dynamic == [0;1;2;3;4;5;6;7;8]
+            SignalInput =  SignalInput + ROITypeTablesub.RoiMean;
+            %size(SignalInput)
+        else
+            ROITypeTablesub = sortrows(ROITypeTablesub,'Dynamic'); %order them according to dynamic, and get the mean from that
+            SignalInput =  SignalInput + ROITypeTablesub.RoiMean;
+        end
+    end
+    SignalInput = SignalInput./SignalInput(1); %normalize to b0
+
+end
