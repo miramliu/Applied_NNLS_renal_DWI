48112df38fff24036964b692f22b7bfd9ccfcbc2
Mira Liu <30845365+miramliu@users.noreply.github.com>
2023-09-21 16:52:35 -0400

    Update DiffusionSpec_Anatomic.m

--- PN_DiffusionSpectra/DiffusionSpec_Anatomic.m
+++ PN_DiffusionSpectra/DiffusionSpec_Anatomic.m
@@ -19,8 +19,14 @@ elseif nargin == 2
     PatientNum = varargin{1};
     RoiTypes = {'LK_LP_C','LK_LP_M','LK_MP_C','LK_MP_M','LK_UP_C','LK_UP_M','RK_LP_C','RK_LP_M','RK_MP_C','RK_MP_M','RK_UP_C','RK_UP_M'};
 
-    if varargin{2} ==1
+    if varargin{2} ==1 %unlabelled
         RoiTypes = {'LP_C','LP_M','MP_C','MP_M','UP_C','UP_M'};
+        ab = 14;
+    elseif varargin{2}==2 %left only
+        RoiTypes = {'LK_LP_C','LK_LP_M','LK_MP_C','LK_MP_M','LK_UP_C','LK_UP_M'};
+        ab = 14;
+    elseif varargin{2}==3
+        RoiTypes = {'RK_LP_C','RK_LP_M','RK_MP_C','RK_MP_M','RK_UP_C','RK_UP_M'};
         ab = 14;
     else
         ab = varargin{2};
@@ -42,30 +48,32 @@ for i = 1:length(RoiTypes)
 
     ROItypename = [PatientNum '_' ROItype];
     %% CHANGE HERE FOR BASELINE, 3M0 OR 12MO
-    SignalInput = ReadPatientDWIData_flexible(PatientNum, ROItypename,ab);
+    %SignalInput = ReadPatientDWIData_flexible(PatientNum, ROItypename,ab);
     %SignalInput = ReadPatientDWIData_3mo(PatientNum, ROItype);
 
+    SignalInput = ReadPatientDWIData_flexible_Arthi(PatientNum, ROItypename,ab); %arthi to compare against... 
     %to match bi-exp, normalizing to b0
     SignalInput = SignalInput(:)/SignalInput(1);
 
 
     %% change line 27 in runnnls_ml to ReadpatientDWIData_3mo for 3mo
+    [~, rsq, ~, ~, resultsPeaks] = RunNNLS_ML(SignalInput);
     %this is restricted now
-    %[~, rsq, ~, ~, resultsPeaks] = RunNNLS_ML_restricted(SignalInput);
+    %[~, rsq, ~, ~, resultsPeaks] = RunNNLS_ML_restricted(SignalInput); %rest
     %[~, rsq, ~, ~, resultsPeaks] = RunNNLS_ML_restricted_both(SignalInput);
 
     %% trying tri-exponential!
-    addpath '/Users/miraliu/Desktop/PostDocCode/Kidney_IVIM'
-    bvals = [0,10,30,50,80,120,200,400,800];
-    [resultsPeaks, rsq] = TriExpIVIMLeastSquaresEstimation_restricted(SignalInput,bvals);
+    %addpath '/Users/miraliu/Desktop/PostDocCode/Kidney_IVIM'
+    %bvals = [0,10,30,50,80,120,200,400,800];
+    %[resultsPeaks, rsq] = TriExpIVIMLeastSquaresEstimation_restricted(SignalInput,bvals);
 
     %plot(OutputDiffusionSpectrum);
     %pause(1)
 
 
     % for redo attempt
-    pathtodata = '/Users/miraliu/Desktop/Data/ML_PartialNephrectomy_Export';
-    ExcelFileName=[pathtodata, '/','PN_IVIM_DiffusionSpectra_TG.xlsx']; % All results will save in excel file
+    pathtodata = '/Users/miraliu/Desktop/Data/Arthi test ROIs';
+    ExcelFileName=[pathtodata, '/','PN_Arthi_IVIM_DiffusionSpectra.xlsx']; % All results will save in excel file
 
     %
     % for baseline 
@@ -77,7 +85,7 @@ for i = 1:length(RoiTypes)
     %ExcelFileName=[pathtodata, '/','PN_IVIM_DiffusionSpectra_3mo.xlsx']; % All results will save in excel file
 
     Identifying_Info = {['PN_' PatientNum], ROItype};
-    Existing_Data = readcell(ExcelFileName,'Range','A:B','Sheet','Rigid_Triexp_Dtissue'); %read only identifying info that already exists
+    Existing_Data = readcell(ExcelFileName,'Range','A:B','Sheet','Sheet1'); %read only identifying info that already exists
     MatchFunc = @(A,B)cellfun(@isequal,A,B);
     idx = cellfun(@(Existing_Data)all(MatchFunc(Identifying_Info,Existing_Data)),num2cell(Existing_Data,2));
 
@@ -85,7 +93,7 @@ for i = 1:length(RoiTypes)
         disp('saving data in excel')
         dataarray= {resultsPeaks(1),resultsPeaks(2),resultsPeaks(3),resultsPeaks(4),resultsPeaks(5),resultsPeaks(6),rsq};
         Export_Cell = [Identifying_Info,dataarray];
-        writecell(Export_Cell,ExcelFileName,'Sheet','Rigid_Triexp_Dtissue','WriteMode','append')
+        writecell(Export_Cell,ExcelFileName,'Sheet','Sheet1','WriteMode','append')
     end
 
 end
