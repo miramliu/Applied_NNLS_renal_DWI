8e8bca2796ea35de29ca55cb414c67e07ac4e6d9
Mira Liu <30845365+miramliu@users.noreply.github.com>
2023-09-21 16:52:45 -0400

    Create InterObserver_Arthi.m

---
+++ PN_DiffusionSpectra/InterObserver_Arthi.m
@@ -0,0 +1,39 @@
+%Arthi double reader check 
+% Arthi ROIs read in IVIM 
+% list of the commands run
+
+% done with RunNNLS_ML and CVNNLS and NNLS_result_mod_ML
+%{
+DiffusionSpec_Anatomic('P001')
+DiffusionSpec_Anatomic('P002',3)
+DiffusionSpec_Anatomic('P003',3)
+DiffusionSpec_Anatomic('P006')
+DiffusionSpec_Anatomic('P007')
+DiffusionSpec_Anatomic('P008')
+DiffusionSpec_Anatomic('P009')
+DiffusionSpec_Anatomic('P010')
+%}
+DiffusionSpec_Anatomic('P012')
+DiffusionSpec_Anatomic('P013')
+DiffusionSpec_Anatomic('P014')
+DiffusionSpec_Anatomic('P015')
+DiffusionSpec_Anatomic('P017')
+DiffusionSpec_Anatomic('P018')
+DiffusionSpec_Anatomic('P019')
+DiffusionSpec_Anatomic('P020')
+DiffusionSpec_Anatomic('P021')
+DiffusionSpec_Anatomic('P022')
+DiffusionSpec_Anatomic('P023')
+DiffusionSpec_Anatomic('P024')
+DiffusionSpec_Anatomic('P025')
+DiffusionSpec_Anatomic('P026')
+DiffusionSpec_Anatomic('P027')
+DiffusionSpec_Anatomic('P028')
+DiffusionSpec_Anatomic('P029')
+DiffusionSpec_Anatomic('P029')
+DiffusionSpec_Anatomic('P030')
+DiffusionSpec_Anatomic('P031')
+DiffusionSpec_Anatomic('P032')
+DiffusionSpec_Anatomic('P033')
+
+
