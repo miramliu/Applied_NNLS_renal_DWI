9cd863443ca61b1b013cb1f26a26f1d79563685e
Mira Liu <30845365+miramliu@users.noreply.github.com>
2023-11-20 17:02:10 -0500

    spectra

--- PN_DiffusionSpectra/RunAll_KidneySpectra.m
+++ PN_DiffusionSpectra/RunAll_KidneySpectra.m
@@ -1,4 +1,5 @@
 
+%{
 DiffusionSpec_Anatomic('P001')
 DiffusionSpec_Anatomic('P002',1) %must change ROI see line 17 in DiffusionSpect_Anatomic
 DiffusionSpec_Anatomic('P003',1) %must change ROI see line 17 in DiffusionSpect_Anatomic
@@ -47,3 +48,54 @@ DiffusionSpec_Anatomic('P042',12) %only 1-2
 
 DiffusionSpec_Anatomic('P043',34) %only 3-4
 DiffusionSpec_Anatomic('P044')
+%}
+%%
+
+DiffusionSpectra_export('P001')
+DiffusionSpectra_export('P002',1) %must change ROI see line 17 in DiffusionSpect_Anatomic
+DiffusionSpectra_export('P003',1) %must change ROI see line 17 in DiffusionSpect_Anatomic
+DiffusionSpectra_export('P004')
+DiffusionSpectra_export('P005')
+DiffusionSpectra_export('P006')
+DiffusionSpectra_export('P007')
+DiffusionSpectra_export('P008')
+DiffusionSpectra_export('P009')
+DiffusionSpectra_export('P010')
+DiffusionSpectra_export('P011')
+DiffusionSpectra_export('P012')
+DiffusionSpectra_export('P013') %13 being funky index error at peak 2
+DiffusionSpectra_export('P014')
+DiffusionSpectra_export('P015')
+DiffusionSpectra_export('P016')
+DiffusionSpectra_export('P017') %17 being funky index error at peak 2
+DiffusionSpectra_export('P018')
+DiffusionSpectra_export('P019')
+DiffusionSpectra_export('P020')
+DiffusionSpectra_export('P021')
+DiffusionSpectra_export('P022')
+DiffusionSpectra_export('P024') %24 being funky index error at peak 2
+DiffusionSpectra_export('P025')
+DiffusionSpectra_export('P026')
+DiffusionSpectra_export('P027')
+DiffusionSpectra_export('P028') %28 being funky index error at peak 2
+DiffusionSpectra_export('P029')
+DiffusionSpectra_export('P030')
+DiffusionSpectra_export('P031')
+DiffusionSpectra_export('P032')
+DiffusionSpectra_export('P033')
+DiffusionSpectra_export('P034')
+%DiffusionSpectra_export('P035',12) %only 1-2, not enough b-values? 
+
+DiffusionSpectra_export('P036',12) %only 1-2
+
+DiffusionSpectra_export('P037',12) %only 1-2
+
+DiffusionSpectra_export('P038',12) %only 1-2
+DiffusionSpectra_export('P039',12) %only 1-2
+
+DiffusionSpectra_export('P040',12) %only 1-2
+
+DiffusionSpectra_export('P042',12) %only 1-2
+
+DiffusionSpectra_export('P043',34) %only 3-4
+DiffusionSpectra_export('P044')
